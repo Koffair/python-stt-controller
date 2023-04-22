4c6aef4dcc3e17e619d7d4d95bfc764548b82007
Gergely Gazda <info@gazdagergo.com>
2023-04-22 17:50:34 +0200

    add json

--- api/index.py
+++ api/index.py
@@ -1,10 +1,11 @@
 from http.server import BaseHTTPRequestHandler
+import json
 
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write({ "hello": "world" })
+        self.wfile.write(json.dumps({'hello': 'world', 'received': 'ok'}))
         return
