dccb8ea5eed5da92d96a03feea6dbc05c3dd4c90
Gergely Gazda <info@gazdagergo.com>
2023-04-22 18:01:10 +0200

    try json again

--- api/index.py
+++ api/index.py
@@ -1,10 +1,17 @@
 from http.server import BaseHTTPRequestHandler
-
+import json
 class handler(BaseHTTPRequestHandler):
 
-    def do_GET(self):
+    def _set_headers(self):
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+        
+    def do_HEAD(self):
+        self._set_headers()
+        
+    # GET sends back a Hello world message
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write(json.dumps({'hello': 'world', 'received': 'ok'}))
         return
