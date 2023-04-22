e6c0f41a7e3bdd321d13e42e4e1cc23383b343bf
Gergely Gazda <info@gazdagergo.com>
2023-04-22 17:58:18 +0200

    remove dircontent

--- api/index.py
+++ api/index.py
@@ -1,12 +1,9 @@
 # main.py
 from fastapi import FastAPI
-import os
 
-dircontent = os.listdir("/var/www/html")
-print(dircontent)
 
 app = FastAPI()
 @app.get("/")
 async def root():
- return {"dircontetn":dircontent}
+ return {"dircontetn": "Hello World"}
  
